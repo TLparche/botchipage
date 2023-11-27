b6de59c148477f25e78b061da01b88514544db82
tlparche <h010508@naver.com>
2023-11-27 22:39:16 +0700

    add secondpage

--- app/page.js
+++ app/page.js
@@ -1,12 +1,11 @@
 import Firstpage from '@/app/components/Firstpage'
+import Secondpage from '@/app/components/Secondpage'
 
 export default function Home() {
     return (
         <div className={"grid"}>
             <Firstpage/>
-            <div>
-                asd
-            </div>
+            <Secondpage/>
         </div>
     )
 }
