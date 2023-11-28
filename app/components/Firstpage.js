9c8f7cc8f4f8f12da4385261d1efe550c2a8109b
tlparche <h010508@naver.com>
2023-11-28 16:31:46 +0700

    add logo

--- app/components/Firstpage.js
+++ app/components/Firstpage.js
@@ -1,9 +1,11 @@
 "use client";
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import YouTubePlayer from '@/app/components/YouTubePlayer';
 
 const Firstpage = () => {
     const [scrollOpacity, setScrollOpacity] = useState(0);
+    const [hidden, setHidden] = useState(false);
+    const [displayNone, setDisplayNone] = useState(false);
 
     const handleScroll = () => {
         const opacity = window.scrollY / 600;
@@ -16,14 +18,34 @@ const Firstpage = () => {
             window.removeEventListener('scroll', handleScroll);
         };
     }, []);
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setHidden(true);
+        }, 1000);
 
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, []);
+    const handleTransitionEnd = () => {
+        if (hidden) {
+            setDisplayNone(true);
+        }
+    };
     return (
         <div>
             <div className="h-screen w-full block relative bg-pink-300">
-                <div className="absolute top-0 left-0 right-0 bg-pink-300 z-30 h-full" style={{ opacity: scrollOpacity }}>
+                <div className="absolute top-0 left-0 right-0 bg-pink-300 z-30 h-full" style={{opacity: scrollOpacity}}>
                 </div>
-                <div className="absolute top-0 left-0 right-0 bg-cover bg-center before:block before:pt-[56.25%] z-20 h-screen">
-                    <YouTubePlayer />
+                <div
+                    className={`h-screen w-full fixed top-0 left-0 right-0 bg-pink-300 transition-opacity duration-1000 z-40 ${
+                        displayNone ? 'hidden' : ''}`}
+                    style={{opacity: hidden ? 0 : 1}} onTransitionEnd={handleTransitionEnd}>
+                    <img src={"kessoku2.png"} alt={"/"} className={"w-full h-screen"}/>
+                </div>
+                <div
+                    className="absolute top-0 left-0 right-0 bg-cover bg-center before:block before:pt-[56.25%] z-20 h-screen">
+                    <YouTubePlayer/>
                 </div>
             </div>
         </div>
