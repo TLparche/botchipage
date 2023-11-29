95e5a939411435f34bd79214ccba957555066184
tlparche <h010508@naver.com>
2023-11-29 16:41:39 +0700

    add footer

---
+++ app/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return(
+        <div className={"h-60 w-full bg-pink-300"}>
+
+        </div>
+    )
+}
+
+export default Footer
--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 import Firstpage from '@/app/components/Firstpage'
 import Secondpage from '@/app/components/Secondpage'
 import Thirdpage from '@/app/components/Thirdpage'
+import Footer from '@/app/components/Footer'
+import React from 'react'
 
 export default function Home() {
     return (
@@ -8,6 +10,7 @@ export default function Home() {
             <Firstpage/>
             <Secondpage/>
             <Thirdpage/>
+            <Footer/>
         </div>
     )
 }
